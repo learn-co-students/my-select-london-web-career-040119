cbcff82780ec4e3ee469319a525e93762001bc81
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-03-26 15:11:18 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  new_collection = []
+
+  while i < collection.length
+    new_collection << collection[i] if yield(collection[i])
+    i += 1
+  end
+  new_collection
 end
+
+
+nums = [1, 2, 3, 4, 5]
+my_select(nums) { |num, value| num.even? }
