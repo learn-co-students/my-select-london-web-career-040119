74d2d2dcc71f54bcbbfd6d20ff61ab9ceec96f31
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-27 22:15:48 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,23 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  if block_given?
+    i = 0
+    new_array = []
+    while i < array.length
+      number = array[i]
+      if number % 2 == 0
+        new_array.push(number)
+      end
+      i += 1
+    end
+    new_array
+  end
 end
+  
+    
+array = [1, 2, 3, 4, 5]
+
+my_select(array) 
+
+# do |number|
+#   number
+# end
